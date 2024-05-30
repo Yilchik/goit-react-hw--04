5b1c41a3f75053956b83d231d7a22c7a3d925fc7
Yilchik <yulyayulia2@gmail.com>
2024-05-30 17:01:45 +0200

    searchBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
+import { useEffect } from "react";
 import "./App.css";
+import axios from "axios";
+import SearchBar from "./components/SearchBar/SearchBar";
 
-function App() {}
+function App() {
+  useEffect(() => {
+    async function searchImages() {
+      const response = await axios.get(
+        "https://api.unsplash.com/photos/?client_id=D_SPVSOx1r7wQQf7tImOfFzoek9ERDaEib9Ux2P1n7M"
+      );
+      console.log(response);
+    }
+    searchImages();
+  }, []);
+  return (
+    <div>
+      <SearchBar></SearchBar>
+    </div>
+  );
+}
 
 export default App;
--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,8 +1,41 @@
+import { Field, Form, Formik } from "formik";
 import css from "./SearchBar.module.css";
+import toast, { Toaster } from "react-hot-toast";
+import * as Yup from "yup";
 
-function SearchBar({ value, onFilter }) {
+function SearchBar({ onSubmit }) {
   return (
-    
+    <header className={css.container}>
+      <Toaster position="top-right" />
+      <Formik
+        initialValues={{ query: "" }}
+        validationSchema={Yup.object({
+          query: Yup.string().required("Please enter a search images"),
+        })}
+        onSubmit={(values, { setSubmitting }) => {
+          if (values.query.trim() === "") {
+            toast.error("Please enter a search images");
+          } else {
+            onSubmit(values.query);
+          }
+          setSubmitting(false);
+        }}
+      >
+        <Form className={css.form}>
+          <Field
+            name="query"
+            type="text"
+            autocomplete="off"
+            autofocus
+            placeholder="Search images and photos"
+            className={css.input}
+          />
+          <button type="submit" className={css.button}>
+            Search
+          </button>
+        </Form>
+      </Formik>
+    </header>
   );
 }
 
