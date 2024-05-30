cd6147cc4e680ccf0721909d8ec869707f2f3a1d
Yilchik <yulyayulia2@gmail.com>
2024-05-30 17:45:55 +0200

    handleSearch

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,32 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import axios from "axios";
 import SearchBar from "./components/SearchBar/SearchBar";
+import ImageGallery from "./components/ImageGallery/ImageGallery";
 
 function App() {
   const [images, setImages] = useState([]);
+  const [query, setQuery] = useState("");
+  const [page, setPage] = useState(1);
 
   useEffect(() => {
-    async function searchImages() {
-      const response = await axios.get(
-        "https://api.unsplash.com/photos/?client_id=D_SPVSOx1r7wQQf7tImOfFzoek9ERDaEib9Ux2P1n7M"
-      );
-      console.log(response);
-      setImages(response.data.hits);
-    }
+    if (!query) return;
+
+    const handleSearch = async function searchImages() {
+      try {
+        const response = await axios.get(
+          "https://api.unsplash.com/photos/?client_id=D_SPVSOx1r7wQQf7tImOfFzoek9ERDaEib9Ux2P1n7M"
+        );
+        console.log(response);
+        setImages(response.data.hits);
+      } catch (error) {
+      } finally {
+      }
+    };
     searchImages();
   }, []);
   return (
     <div>
-      <SearchBar></SearchBar>
+      <SearchBar onSubmit={handleSearch} />
       {images.length > 0 && <ImageGallery items={images} />}
     </div>
   );
--- src/components/ImageCard/ImageCard.jsx
+++ src/components/ImageCard/ImageCard.jsx
@@ -1,16 +1,19 @@
 import css from "./ImageCard.module.css";
 
-
-function ImageCard({ image, onImageClick }) => {
+function ImageCard({ image, onImageClick }) {
   return (
     <div className={css.card} onClick={() => onImageClick(image)}>
-      <img src={image.urls.small} alt={image.alt_description} className={css.image} />
+      <img
+        src={image.urls.small}
+        alt={image.alt_description}
+        className={css.image}
+      />
       <div className={css.overlay}>
         <p>{image.user.name}</p>
         <p>{image.likes} likes</p>
       </div>
     </div>
   );
-};
+}
 
 export default ImageCard;
