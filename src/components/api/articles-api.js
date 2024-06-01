220efa77522a9b097f1109e8c00e4eef72e30571
Yilchik <yulyayulia2@gmail.com>
2024-06-01 22:33:19 +0200

    headers

--- src/components/api/articles-api.js
+++ src/components/api/articles-api.js
@@ -3,10 +3,12 @@ import axios from "axios";
 axios.defaults.baseURL = "https://api.unsplash.com";
 
 export const getArticlesApi = async (searchQuery) => {
-  const { data } = await axios.get("/photos", {
+  const { data } = await axios.get("/search/photos", {
+    headers: {
+      Authorization: `Client-ID ${process.env.REACT_APP_Access_Key}`,
+    },
     params: {
       query: searchQuery,
-      client_id: process.env.REACT_APP_Access_Key,
     },
   });
   return data.hits;
