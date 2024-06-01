1b80fe85070253a72c14522671034dcca09d897d
Yilchik <yulyayulia2@gmail.com>
2024-06-01 17:36:58 +0200

    loader

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import ImageGallery from "./components/ImageGallery/ImageGallery";
 
 function App() {
   const [images, setImages] = useState([]);
+  const [loading, setLoading] = useState(false);
   const [query, setQuery] = useState("");
   const [page, setPage] = useState(1);
 
@@ -14,6 +15,7 @@ function App() {
 
     const handleSearch = async function searchImages() {
       try {
+        setLoading(true);
         const response = await axios.get(
           "https://api.unsplash.com/photos/?client_id=D_SPVSOx1r7wQQf7tImOfFzoek9ERDaEib9Ux2P1n7M"
         );
@@ -21,13 +23,15 @@ function App() {
         setImages(response.data.hits);
       } catch (error) {
       } finally {
+        setLoading(false);
       }
     };
-    searchImages();
+    handleSearch();
   }, []);
   return (
     <div>
       <SearchBar onSubmit={handleSearch} />
+      {loading && <p>Loading images, please wait...</p>}
       {images.length > 0 && <ImageGallery items={images} />}
     </div>
   );
