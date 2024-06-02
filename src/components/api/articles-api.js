528128b8dac15d7daf3785f9620298c7ac0f59f6
Yilchik <yulyayulia2@gmail.com>
2024-06-02 11:20:57 +0200

    api key

--- src/components/api/articles-api.js
+++ src/components/api/articles-api.js
@@ -1,11 +1,12 @@
 import axios from "axios";
 
 axios.defaults.baseURL = "https://api.unsplash.com";
+const Api_Key = process.env.REACT_APP_Access_Key;
 
 export const getArticlesApi = async (searchQuery) => {
   const { data } = await axios.get("/search/photos", {
     headers: {
-      Authorization: `Client-ID ${process.env.REACT_APP_Access_Key}`,
+      Authorization: `Client-ID ${Api_Key}`,
     },
     params: {
       query: searchQuery,
