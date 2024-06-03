71058b891a11242d6345c8fa41e5dcedc7865de5
Yilchik <yulyayulia2@gmail.com>
2024-06-03 11:28:56 +0200

    onClick

--- src/components/ImageCard/ImageCard.jsx
+++ src/components/ImageCard/ImageCard.jsx
@@ -2,11 +2,12 @@ import css from "./ImageCard.module.css";
 
 function ImageCard({ image, onImageClick }) {
   return (
-    <div className={css.card} onClick={() => onImageClick(image)}>
+    <div className={css.card}>
       <img
         src={image.urls.small}
         alt={image.alt_description}
         className={css.image}
+        onClick={() => onImageClick(image)}
       />
       <div className={css.overlay}>
         <p>Author: {image.user.name}</p>
